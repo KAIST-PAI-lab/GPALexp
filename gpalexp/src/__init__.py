7ca6d999067de435b382273979104222c8fb1214
Junyup Kim <ytrewq271828@naver.com>
2025-09-16 14:58:20 +0900

    src directory added

--- gpalexp/src/__init__.py
+++ gpalexp/src/__init__.py
@@ -1,14 +1,15 @@
 from .gpal_optimize import gpal_optimize
 from .gpr_instance import GPRInstance
 from .utils import argsConstructor, sequence_with_interval, grid_with_sequences
-from .gpal_plot_new import plot_GP, plot_selection_frequency, plot_convergence, plot_convergence_and_frequency
+from .gpal_plot import plot_convergence, plot_frequency_histogram_1D, plot_frequency_histogram_2D, plot_GPAL_compare_uncertainty, plot_GPAL_uncertainty
 
 __all__ = ['gpal_optimize', 
            'GPRInstance', 
            'argsConstructor',
            'sequence_with_interval',
            'grid_with_sequences',
-           'plot_GP',
-           'plot_selection_frequency',
            'plot_convergence',
-           'plot_convergence_and_frequency']
+           'plot_frequency_histogram_1D',
+           'plot_frequency_histogram_2D',
+           'plot_GPAL_compare_uncertainty',
+           'plot_GPAL_uncertainty']
